eb35fd78b2dc39fba3d54a62b623846d7a78f0bf
Kazi Mishkatul Islam <66875314+kazimishkat@users.noreply.github.com>
2026-01-22 12:25:37 +0600

    Update 21-jan-26-exam_2.sql

--- 21-jan-26-exam_2.sql
+++ 21-jan-26-exam_2.sql
@@ -1,4 +1,4 @@
-create a table and name using your id 
+1.create a table and name using your id 
 preceded by emp (as emp106588),which have
 5 field as eid,ename,salary,hire_date and 
 address with appropriate data types and create
@@ -6,15 +6,25 @@ primary key on eid.
 
 and insert any 5 row.
 
-displays the department numbers and average salaries for those departments with 
+2.displays the department numbers and average salaries for those departments with 
 maximum salary that is greater than 12000?
 
-display the name whose name start from 's'?
+3.display the name whose name start from 's'?
 
-display the avg salary of employees of department sales?
+4.display the avg salary of employees of department sales?
 
 
-select department_id, avg(salary)
-from employees
-group by departments
-where max salary>12000;
+ans:2. 
+select department_id,avg(salary)
+from EMPLOYEES
+group by department_id
+having max(salary)>12000;
+
+ans:3.
+SELECT first_name FROM employees WHERE first_name LIKE 'S%';
+
+ans:4,
+SELECT AVG(salary) AS avg_salary 
+FROM employees
+WHERE department_id = 
+  ( SELECT department_id FROM departments WHERE department_name = 'Sales' );
